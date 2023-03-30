9912900d3050a6427faac4468977d2643480c79d
Umayanga Alahakoon <umayanga@usa.com>
2023-03-30 17:53:14 +0530

    Initial Commit

---
+++ SwiftUIChatGPT/SwiftUIChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatGPTApp.swift
+//  SwiftUIChatGPT
+//
+//  Created by Umayanga Alahakoon on 2023-03-30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
